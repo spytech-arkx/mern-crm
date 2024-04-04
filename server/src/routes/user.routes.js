a49ea4189f9b68578866dde696ef21c71837c14d
ZakErGithub <zak.errahil17@gmail.com>
2024-04-04 15:07:03 +0000

    Add login route & require avoid authentication  middlware

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -1,17 +1,20 @@
 const express = require('express');
 const { validateParamsId, validateBodyData } = require('../middlewares/validator');
 const sanitizeBodyData = require('../middlewares/sanitizer');
+const {avoidAuth} = require("../middlewares/avoid.authentication")
 const {
   getUsers,
   getUserById,
   deleteUser,
   updateUser,
   createUsers,
+  loginUser,
 } = require('../controllers/user.controller');
 
 const userRouter = express.Router();
 
 userRouter.post('/', sanitizeBodyData, validateBodyData, createUsers);
+userRouter.post('/auth/login', avoidAuth, loginUser);
 userRouter.get('/', getUsers);
 userRouter.get('/:id', validateParamsId, getUserById);
 userRouter.patch(
