a70ecbedaeb714767fb5fcebf901989fff3fb1cf
fashyl <souhailfaraji@gmail.com>
2024-04-18 16:40:37 +0100

    update error and type handlers Because of opting for session based authentication instead of jwt. add new entity(lead) to schemaChoiceHandler and typeHandler

--- server/src/helpers/typeHandler.js
+++ server/src/helpers/typeHandler.js
@@ -1,3 +1,5 @@
+const { logger } = require('../utils/logger');
+
 module.exports = (url) => {
   if (/companies/.test(url)) return 'company';
   if (/tasks/.test(url)) return 'task';
@@ -5,5 +7,7 @@ module.exports = (url) => {
   if (/deals/.test(url)) return 'deal';
   if (/login/.test(url)) return 'login';
   if (/users/.test(url)) return 'user';
+  if (/leads/.test(url)) return 'lead';
+  logger.log('error', "grave: Couldn't parse type from url");
   throw new Error('Invalid type');
 };
