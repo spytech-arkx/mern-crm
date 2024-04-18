a29170c8baa2bb6da4a3316c09b9a5c0f76bba1e
fashyl <souhailfaraji@gmail.com>
2024-04-18 16:52:00 +0100

    Add User registration (issue #1)

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -1,72 +1,55 @@
-const { readUsers, writeUsers, getUserByEmail } = require('../services/db/user.service');
-// const { comparePassword, hashPassword } = require('../helpers/hashing');
-const { generateToken } = require('../helpers/jwt');
 const handleError = require('../helpers/errorHandler');
+const User = require('../models/user.model');
+const { readUsers, writeUsers, updateWithoutReturn } = require('../services/db/user.service');
 
-exports.loginUser = async (req, res) => {
-  try {
-    const { email, password } = req.body;
-    const user = await getUserByEmail(email);
-    if (!user) return res.status(400).json({ message: 'User not found' });
-    // const checked = await comparePassword(password, user.password);
-
-    if (user.password === password) {
-      const newUser = { ...user }.doc; // extracting user data
-      delete newUser.password; // Remove the password
-      const token = await generateToken(newUser);
-      res.cookie('tokenAuth', token);
-      return res.status(302).json({ message: 'Logged in' });
-    } // ila makanch ?
-    return res.status(403).json({ type: 'Forbideen', message: 'Invalid credentials' });
-  } catch (err) {
-    return handleError(err, res);
-  }
-};
-exports.logout = (req, res) => {
-  res.cookie('tokenAuth', '', { expires: new Date(0) });
-  res.status(200).send('Logout successful.');
-};
-
-exports.logout = (req, res) => {
-  res.cookie('tokenAuth', '', { expires: new Date(0) });
-  return res.status(200).send('Logout successful.');
-};
-
+// Admin priv. required
 exports.getUsers = async (req, res) => {
   try {
-    const companies = await readUsers({}, { createdAt: 0, modifiedAt: 0 });
-    res.status(200).json({
-      type: 'read_all',
-      items: companies.length ? companies : 'Nothing here :/',
-    });
+    const users = await readUsers({}, { createdAt: 0, modifiedAt: 0 });
+    res.status(200).json(users.length ? users : 'Nothing here :/');
   } catch (err) {
     handleError(err, res);
   }
 };
 
+// Admin priv. required
 exports.getUserById = async (req, res) => {
   try {
-    const companies = await readUsers(
-      { _id: req.params.id },
-      { createdAt: 0, modifiedAt: 0 },
-    );
-    if (!companies.length) {
-      return res
-        .status(404)
-        .json({ type: 'ErrorNotFound', message: 'User not found :/' });
-    }
-    return res.status(200).json({ type: 'read_one', item: companies[0] });
+    const users = await readUsers({ _id: req.params.id });
+    if (!users.length) return res.status(404).json({ type: 'ErrorNotFound', message: 'User not found :/' });
+    return res.status(200).json(users[0]);
   } catch (err) {
     return handleError(err, res);
   }
 };
 
+// Admin priv. required
 exports.createUsers = async (req, res) => {
   try {
     const writeData = await writeUsers(req.body, 'insertOne');
-    res
-      .status(201)
-      .json({ type: 'write_insert', result: writeData, message: 'Created.' });
+    res.status(201).json({ result: writeData, message: 'Created.' });
+  } catch (err) {
+    handleError(err, res);
+  }
+};
+
+/**
+ * The register function registers a new user.
+ * @param {Object} req- The request object.
+ * @param {Object} res - The response.
+ */
+exports.register = async (req, res) => {
+  try {
+    const newUser = new User({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      username: req.body.username,
+      password: req.body.password,
+    });
+    const user = await newUser.save();
+    res.status(201).json(user);
+    // res.redirect('/login');
   } catch (err) {
     handleError(err, res);
   }
@@ -74,31 +57,19 @@ exports.createUsers = async (req, res) => {
 
 exports.updateUser = async (req, res) => {
   try {
-    const writeData = await writeUsers(req.body, 'updateOne', { _id: req.params.id });
-    if (!writeData.modifiedCount) {
-      return res
-        .status(404)
-        .json({ type: 'ErrorNotFound', message: 'User not found :/' });
-    }
-    return res
-      .status(200)
-      .json({ type: 'write_update', result: writeData, message: 'Updated.' });
+    await updateWithoutReturn(req.params.id, req.body);
+    return res.status(200).json({ message: 'Updated.' });
   } catch (err) {
     return handleError(err, res);
   }
 };
 
+// TODO: User deactivation, soft-deletion.
 exports.deleteUser = async (req, res) => {
   try {
     const writeData = await writeUsers({}, 'deleteOne', { _id: req.params.id });
-    if (!writeData.deletedCount) {
-      return res
-        .status(404)
-        .json({ type: 'ErrorNotFound', message: 'User not found :/' });
-    }
-    return res
-      .status(200)
-      .json({ type: 'write_delete', result: writeData, message: 'Deleted.' });
+    if (!writeData.deletedCount) return res.status(404).json({ type: 'ErrorNotFound', message: 'User not found :/' });
+    return res.status(200).json({ result: writeData, message: 'Deleted.' });
     // 204 : No Content actually returns no content..
   } catch (err) {
     return handleError(err, res);
