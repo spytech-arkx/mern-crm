52a167961aa2bbcdf7d9148a52505b9507ca6b88
fashyl <souhailfaraji@gmail.com>
2024-04-18 17:09:05 +0100

    separate bulk editing and single user updates

--- server/src/services/db/user.service.js
+++ server/src/services/db/user.service.js
@@ -2,27 +2,22 @@ const User = require('../../models/user.model');
 
 async function readUsers(filter, projection, options) {
   try {
-    return await User.find(filter, projection, options); // Return the response
+    return await User.find(filter, projection, options);
   } catch (err) {
     throw err;
   }
 }
 
-async function getUserByEmail(email) {
-  try {
-    const filter = { email: email }; // Filter by the specified email
-    const projection = { email: 1, password: 1 }; // Include email and password fields
-    const user = await User.find(filter, projection); // Return the response
-    // const user = await readUsers({email}, { email: 1, password: 1 })
-    // Check if any user was found
-    if (user) {
-      return user[0];
-    }
-    return null;
-  } catch (err) {
-    throw err;
-  }
-}
+// A note on insertOne vs. save()
+//    save() is and always have been the preferred approach when adding docs to the db.
+//    It provides validation and middleware and the entire update happens as a single unit.
+//    So why use insertOne instead ? two reasons:
+//        1. Justify the use of Joi for validation.
+//        2. Simple, slightly fast insertions to the db.
+//    This said, chances of bulkwriting users are slim,
+//    For now, registering users uses save(), 'validated twice ¯\_(ツ)_/¯'
+//    See: ../../controllers/user.controller.js#register}
+//    updating users uses updateOne, see below.
 
 async function writeUsers(docs, operation, filters) {
   try {
@@ -44,8 +39,16 @@ async function writeUsers(docs, operation, filters) {
   }
 }
 
+async function updateWithoutReturn(userId, update) {
+  try {
+    return await User.updateOne({ _id: userId }, { $set: update });
+  } catch (error) {
+    throw error;
+  }
+}
+
 module.exports = {
   readUsers,
   writeUsers,
-  getUserByEmail,
+  updateWithoutReturn,
 };
