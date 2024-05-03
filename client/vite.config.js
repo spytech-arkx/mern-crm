e7592612c33907237443a6843d5d16ea66d529bb
Souhail Faraji <152604090+fashyl@users.noreply.github.com>
2024-05-03 22:27:13 +0100

    (fix) @ aliases  not resolving

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,14 +1,17 @@
 import { defineConfig } from 'vite'
-import path from 'path'
+import * as path from 'path'
 import react from '@vitejs/plugin-react'
+import {fileURLToPath} from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: {
-      // eslint-disable-next-line no-undef
-      "@": path.resolve(__dirname, "./src"),
-    },
+    alias: [
+      { find: '@', replacement: path.resolve(__dirname, 'src') },
+    ],
   },
 })
