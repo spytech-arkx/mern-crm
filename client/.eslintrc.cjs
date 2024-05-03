877d5d4e90bd7e4f60019c233ba56aeff369c6c4
Souhail Faraji <152604090+fashyl@users.noreply.github.com>
2024-05-03 22:28:20 +0100

    update lint & formatting rules

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true, node: true },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
@@ -9,14 +9,30 @@ module.exports = {
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
+  settings: {
+    react: { version: '18.2' },
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.vue', '.ts', '.d.ts'],
+      },
+      alias: {
+        extensions: ['.vue', '.js', '.ts', '.scss', '.d.ts', '.jsx'],
+        map: [
+          ['@/components', './src/components'],
+          ['@/pages', './src/pages'],
+          ['@/router', './src/router'],
+          ['@/store', './src/store'],
+          ['@/styles', './src/styles'],
+          ['@/types', './src/types'],
+          ['@/utils', './src/utils'],
+        ],
+      },
+    },
+  },
   plugins: ['react-refresh'],
   rules: {
     'react/jsx-no-target-blank': 'off',
     'react/prop-types': 'off',
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
   },
-}
+};
