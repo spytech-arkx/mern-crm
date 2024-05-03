c935838948029bb81ac9e2be00825083f639f1f5
Souhail Faraji <152604090+fashyl@users.noreply.github.com>
2024-05-03 22:32:58 +0100

    scaffold basic ui with mock tasks data (w/ actions)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,10 @@
-import CompanyList from './components/entities/companies/company-list'
-import ContactList from './components/entities/contacts/contact-list'
-import DealList from './components/entities/deals/deal-list'
 import TaskList from './components/entities/tasks/task-list'
 import './styles/index.css'
 
 function App() {
   return (
     <>
-    <ContactList />
     <TaskList />
-    <DealList />
-    <CompanyList />
     </>
   )
 }
--- client/src/components/entities/tasks/task-list.jsx
+++ client/src/components/entities/tasks/task-list.jsx
@@ -1,11 +1,49 @@
-import { DataTable } from "../../data-table"
-import { taskColumns } from "./task-columns"
-import { tasks } from "../../../assets/mock-data"
+// import { z } from "zod"
+
+import { columns } from "./columns"
+import { DataTable } from "./data-table"
+// import { taskSchema } from "@/data/tasks/schema"
+import data from '@/data/tasks/tasks.json'
+
+// Simulate a database read for tasks.
+// async function getTasks() {
+//   return z.array(taskSchema).parse(data)
+// }
 
 export default function TaskList() {
+
   return (
-    <div className="container mx-auto py-10">
-      <DataTable columns={taskColumns} data={[...tasks]} />
-    </div>
+    <>
+      <div className="md:hidden">
+        <img
+          src="/examples/tasks-light.png"
+          width={1280}
+          height={998}
+          alt="Playground"
+          className="block dark:hidden"
+        />
+        <img
+          src="/examples/tasks-dark.png"
+          width={1280}
+          height={998}
+          alt="Playground"
+          className="hidden dark:block"
+        />
+      </div>
+      <div className="hidden h-full flex-1 flex-col space-y-8 p-8 md:flex">
+        <div className="flex items-center justify-between space-y-2">
+          <div>
+            <h2 className="text-2xl font-bold tracking-tight">Welcome back!</h2>
+            <p className="text-muted-foreground">
+              Here&apos;s a list of your tasks for this month!
+            </p>
+          </div>
+          {/* <div className="flex items-center space-x-2">
+            <UserNav />
+          </div> */}
+        </div>
+        <DataTable data={data} columns={columns} />
+      </div>
+    </>
   )
 }
