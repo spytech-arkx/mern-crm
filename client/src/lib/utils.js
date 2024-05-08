0f0585c6c8acbc7c9a08e87681ef4bf6f15dd5eb
Souhail Faraji <152604090+fashyl@users.noreply.github.com>
2024-05-08 18:00:03 +0100

    add formatter for displayed dates

--- client/src/lib/utils.js
+++ client/src/lib/utils.js
@@ -4,3 +4,10 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs) {
   return twMerge(clsx(inputs))
 }
+
+export const formatter = new Intl.DateTimeFormat('en-GB', {
+  year: '2-digit',
+  day: '2-digit',
+  month: 'short',
+  weekday: 'short',
+});
