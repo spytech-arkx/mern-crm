c68bcf95b4868bea5a0fd7e1af90f788a12d2028
Souhail Faraji <152604090+fashyl@users.noreply.github.com>
2024-05-17 14:32:35 +0100

    (fix) remove node modules

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,17 +1,18 @@
 import { defineConfig } from 'vite'
 import * as path from 'path'
 import react from '@vitejs/plugin-react'
-import {fileURLToPath} from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ["fs", "url"],
+    }
+  },
   resolve: {
-    alias: [
-      { find: '@', replacement: path.resolve(__dirname, 'src') },
-    ],
+    alias: {
+      '@': path.resolve(import.meta.dirname, './src'),
+    },
   },
 })
