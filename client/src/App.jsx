a58e0ac17806dd266bf4837c4560378c2e2d53d8
Souhail Faraji <152604090+fashyl@users.noreply.github.com>
2024-05-19 00:46:14 +0100

    (to-do) do this for the remaining entities O.o

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,15 @@
-import TaskList from './components/entities/tasks/task-list'
+import TaskList from './components/entities/tasks/list'
+import { Toaster } from '@/components/ui/sonner'
 import './styles/index.css'
+import { TaskFormDrawer } from './components/entities/tasks/form-drawer'
 
 function App() {
   return (
-    <>
+    <div>
     <TaskList />
-    </>
+    <TaskFormDrawer />
+    <Toaster />
+    </div>
   )
 }
 
