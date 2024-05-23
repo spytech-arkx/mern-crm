445355dda4ca3d27330d20ed08ff0d6f4742ddaf
Souhail Faraji <152604090+fashyl@users.noreply.github.com>
2024-05-23 02:29:47 +0100

    add redis for session management

--- server/src/config/session.js
+++ server/src/config/session.js
@@ -1,20 +1,38 @@
-const session = require('express-session');
-const MongoStore = require('connect-mongo');
+const session = require("express-session");
+const { createClient } = require("redis");
+const MongoStore = require("connect-mongo");
+const { default: RedisStore } = require("connect-redis");
 const connection = require('./db');
-require('dotenv').config();
+const { logger } = require("../utils/logger");
+require("dotenv").config();
+
+// const redisClient = createClient();
+// redisClient
+//   .connect()
+//   .then(() => logger.log("info", "[Redis] client is up and running.."))
+//   .catch(() => { logger.log("error", "[Redis] failed connect\n")});
+
+// const redisStore = new RedisStore({
+//   client: redisClient,
+//   prefix: 'snazCRM:',
+// });
+
+const mongoStore = new MongoStore({
+  mongoUrl: process.env.MONGODB_URI,
+  mongooseConnection: connection,
+  collectionName: 'sessions',
+});
 
 module.exports = session({
+  name: "snaz_0ozQd8310",
   secret: process.env.SECRET_KEY,
   resave: false,
   saveUninitialized: false,
-  store: new MongoStore({
-    mongoUrl: process.env.MONGODB_URI,
-    mongooseConnection: connection,
-    collectionName: 'sessions',
-  }),
+  unset: "destroy",
+  store: mongoStore,
   cookie: {
     httpOnly: true,
-    maxAge: 1000 * 60 * 60 * 24,
+    maxAge: 1000 * 60 * 10,
   },
 });
 
