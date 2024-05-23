3ea412c59380532759ff232e7ea47ad88f1c65c1
Souhail Faraji <152604090+fashyl@users.noreply.github.com>
2024-05-23 02:52:19 +0100

    prevent password divulgence --'

--- server/src/config/passport.js
+++ server/src/config/passport.js
@@ -1,31 +1,38 @@
-const passport = require('passport');
-const bcrypt = require('bcrypt');
-const LocalStrategy = require('passport-local').Strategy;
-const User = require('../models/user.model');
+const passport = require("passport");
+const bcrypt = require("bcrypt");
+const LocalStrategy = require("passport-local").Strategy;
+const User = require("../models/user.model");
 
-const local = new LocalStrategy({
-  usernameField: 'email',
-  passwordField: 'password',
-}, async (email, password, done) => {
-  try {
-    const user = await User.findOne({ email });
-    if (!user) return done(null, false, { message: 'Incorrect Email.' });
+const local = new LocalStrategy(
+  {
+    usernameField: "email",
+    passwordField: "password",
+  },
+  (async (email, password, done) => {
+    try {
+      const user = await User.findOne({ email });
+      if (!user) return done(null, false, { message: "Incorrect Email." });
 
-    const match = await bcrypt.compare(password, user.password);
-    if (!match) return done(null, false, { message: 'Incorrect Password.' });
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) return done(null, false, { message: "Incorrect Password." });
 
-    return done(null, user);
-  } catch (err) {
-    return done(err);
-  }
-});
+      delete user.password;
+      delete user.__v;
+
+      return done(null, user);
+    } catch (err) {
+      return done(err);
+    }
+  }),
+);
 
 passport.use(local);
 passport.serializeUser((user, done) => {
-  done(null, user.id);
+  user.password = "hidden"; // call me lazy.
+  done(null, user);
 });
 passport.deserializeUser((id, done) => {
-  User.findOne({ _id: id })
+  User.findById(id)
     .then((user) => done(null, user))
     .catch((err) => done(err));
 });
